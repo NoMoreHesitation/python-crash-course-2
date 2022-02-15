ccf72389355286426e754d325a7065eb467b978b
Ziyang <cziyang08@gmail.com>
2022-02-15 19:17:01 +0100

    json存储内容

---
+++ json_use.py
@@ -0,0 +1,13 @@
+import json
+
+filename = 'user_name.json'
+try:
+	with open(filename) as f:
+		username = json.load(f)
+except FileNotFoundError:
+	username = input('tell me your name:')
+	with open(filename, 'w') as f:
+		json.dump(filename, f)
+		print(f'We will remember you when you come back,{username}')
+else:
+	print(f'Welcome back,{username}!')
