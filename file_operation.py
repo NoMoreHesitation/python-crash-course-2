55f85bf00b3cb90d238a84c529ead57a48752589
Ziyang <cziyang08@gmail.com>
2022-02-15 18:30:23 +0100

    读写文件操作

--- file_operation.py
+++ file_operation.py
@@ -3,12 +3,10 @@
 with open('test_input.txt', encoding='utf-8') as file_input:
 	content = file_input.read()
 	print(content.rstrip())
-
 # 一次打印一行
 with open('test_input.txt', encoding='utf-8') as file_input:
 	for contents in file_input:
 		print(contents.rstrip())
-
 # 存入中间变量，后面再用
 with open('test_input.txt', encoding='utf-8') as file_input:
 	lines = file_input.readlines()
@@ -16,3 +14,12 @@ file_string = ''
 for line in lines:
 	file_string += line.strip()
 print(file_string)
+
+# 写入文件，w表示写入,数字要转换
+file_name = 'test_output.txt'
+with open(file_name, 'w') as file_output:
+	file_output.write('I love programming.\n')
+	file_output.write(str(2)+'\n')
+# 附加模式，在原来文件基础上添加
+with open(file_name, 'a') as file_add:
+	file_add.write('I love playing computer games!')
