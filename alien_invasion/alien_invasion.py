378cc651512dd52068d3571bbb5bc5f19cfca0f1
Ziyang <cziyang08@gmail.com>
2022-02-23 17:08:09 +0100

    初始化外星人游戏背景和大小

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+
+
+class AlienInvasion:
+	"""
+	管理游戏资源和行为
+	"""
+
+	def __init__(self):
+		"""
+		初始化游戏并创建游戏资源
+		"""
+		pygame.init()
+		self.screen = pygame.display.set_mode((800, 600))
+		# RGB色光三原色
+		self.bcakground_color = (80, 120, 255)
+		pygame.display.set_caption('Alien Invasion')
+
+	def run_game(self):
+		"""
+		游戏的主循环
+		:return:
+		"""
+		while True:
+			# 监视键盘鼠标
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+			# 每次绘制新屏幕，擦去旧屏幕
+			self.screen.fill(self.bcakground_color)
+			pygame.display.flip()
+
+
+if __name__ == '__main__':
+	ai = AlienInvasion()
+	ai.run_game()
