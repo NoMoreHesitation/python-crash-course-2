706b76845c7444f65679379bd12134f97af2d14c
Ziyang <cziyang08@gmail.com>
2022-02-24 18:20:53 +0100

    json修改语句

--- json_use.py
+++ json_use.py
@@ -7,7 +7,7 @@ try:
 except FileNotFoundError:
 	username = input('tell me your name:')
 	with open(filename, 'w') as f:
-		json.dump(filename, f)
+		json.dump(username, f)
 		print(f'We will remember you when you come back,{username}')
 else:
 	print(f'Welcome back,{username}!')
